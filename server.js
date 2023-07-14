ee15f4be619aef332748e4ee63d2a8c6e4bfb30a
Louay Attahiri <louay-attahiri@hotmail.com>
2023-07-14 10:41:11 +0200

    lllllllllll
    ssssllllllllll

--- server.js
+++ server.js
@@ -36,71 +36,83 @@ app.post("/create-checkout-session", async (req, res) => {
         },
       ],
 
-      success_url: `https://taxicentralschiphol.nl/succes-payment/`,
+      success_url: `https://taxicentralschiphol.nl/success-payment/?session_id={CHECKOUT_SESSION_ID}`,
       cancel_url: `https://taxicentralschiphol.nl`,
     });
 
+    res.json({ url: session.url });
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+});
+
+// Endpoint to handle successful payments
+app.get("/success-payment", async (req, res) => {
+  try {
+    const session = await stripe.checkout.sessions.retrieve(
+      req.query.session_id
+    );
+    const paymentIntent = await stripe.paymentIntents.retrieve(
+      session.payment_intent
+    );
+
+    // Email sending logic
     const transporter = nodemailer.createTransport({
-      host: "mail.taxicentralschiphol.nl",
+      host: "mail.booktaxinow.nl",
       port: 587,
       secure: false,
       auth: {
-        user: "info@taxicentralschiphol.nl",
-        pass: "info",
+        user: "test@booktaxinow.nl",
+        pass: "test",
       },
       tls: {
         rejectUnauthorized: false, // Disable hostname verification (not recommended for production)
         ciphers: "TLSv1.2",
       },
     });
-
-    // Create the email message
     const mailOptions = {
-      from: "info@taxicentralschiphol.nl",
-      to: ["info@taxicentralschiphol.nl"],
+      from: "attahirilouay@gmail.com",
+      to: ["attahirilouay@gmail.com"],
       subject: "Payment Successful",
       html: `
-      <html>
-        <head>
-          <style>
-            body {
-              font-family: Arial, sans-serif;
-              margin: 0;
-              padding: 0;
-            }
-
-            h1 {
-              color: #333333;
-            }
-
-            ul {
-              list-style-type: none;
-              padding: 0;
-            }
-
-            li {
-              margin-bottom: 10px;
-            }
-          </style>
-        </head>
-        <body>
-          <h1>Payment Successful</h1>
-          <p>Thank you for your payment. Here are the details:</p>
-          <ul>
-            <li><strong>Name:</strong> ${req.body.name}</li>
-            <li><strong>Email:</strong> ${req.body.email}</li>
-            <li><strong>Phone:</strong> ${req.body.phone}</li>
-            <li><strong>Pickup Location:</strong> ${req.body.pickup}</li>
-            <li><strong>Dropoff Location:</strong> ${req.body.dropoff}</li>
-            <li><strong>Distance:</strong> ${req.body.distance}</li>
-            <li><strong>Duration:</strong> ${req.body.duration}</li>
-            <li><strong>Price:</strong> ${req.body.price_data.currency} ${req.body.price_data.unit_amount}</li>
-          </ul>
-        </body>
-      </html>
-    `,
+        <html>
+          <head>
+            <style>
+              body {
+                font-family: Arial, sans-serif;
+                margin: 0;
+                padding: 0;
+              }
+              h1 {
+                color: #333333;
+              }
+              ul {
+                list-style-type: none;
+                padding: 0;
+              }
+              li {
+                margin-bottom: 10px;
+              }
+            </style>
+          </head>
+          <body>
+            <h1>Payment Successful</h1>
+            <p>Thank you for your payment. Here are the details:</p>
+            <ul>
+              <li><strong>Name:</strong> ${paymentIntent.metadata.name}</li>
+              <li><strong>Email:</strong> ${paymentIntent.metadata.email}</li>
+              <li><strong>Phone:</strong> ${paymentIntent.metadata.phone}</li>
+              <li><strong>Pickup Location:</strong> ${paymentIntent.metadata.pickup}</li>
+              <li><strong>Dropoff Location:</strong> ${paymentIntent.metadata.dropoff}</li>
+              <li><strong>Distance:</strong> ${paymentIntent.metadata.distance}</li>
+              <li><strong>Duration:</strong> ${paymentIntent.metadata.duration}</li>
+              <li><strong>Price:</strong> ${paymentIntent.currency} ${paymentIntent.amount}</li>
+            </ul>
+          </body>
+        </html>
+      `,
     };
-    // Send the email
+
     transporter.sendMail(mailOptions, (error, info) => {
       if (error) {
         console.log("Error sending email:", error);
@@ -109,7 +121,7 @@ app.post("/create-checkout-session", async (req, res) => {
       }
     });
 
-    res.json({ url: session.url });
+    res.redirect("https://taxicentralschiphol.nl/success-payment.html");
   } catch (e) {
     res.status(500).json({ error: e.message });
   }
